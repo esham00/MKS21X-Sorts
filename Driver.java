c5157f89572b59b1a9ed4ad8bd5fc757aa5d167d
Elizabeth Sham <esham00@stuy.edu>
2018-12-09 13:50:07 -0500

    2x runtime (changed driver bc it was testing improperly)

--- Driver.java
+++ Driver.java
@@ -3,18 +3,16 @@ public class Driver {
     public static void main (String[] args) {
 	if (args.length > 0) {
 	    Random num = new Random();
-	    for(int i = 0; i < Integer.parseInt(args[0]); i++) {
-		int[] ary =  new int[10];
-		for (int j = 0; j < ary.length; j++) {
+	    int[] ary =  new int[Integer.parseInt(args[0])];
+	    for (int j = 0; j < ary.length; j++) {
 		    ary[j] = num.nextInt()%10;
-		}
-		//System.out.println(Arrays.toString(ary));
-		 Sorts.bubbleSort(ary);
-		// System.out.println(Arrays.toString(ary));
 	    }
+	    //System.out.println(Arrays.toString(ary));
+	    Sorts.selectionSort(ary);
+	    // System.out.println(Arrays.toString(ary));
 	}
 	else {
-	    System.out.println("input the amount of times you wanna run sort");
+	    System.out.println("input the length of the array");
 	}
     }
 }

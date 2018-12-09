6047a394b4282618d7d7efd942f071a6f60ed1ef
Elizabeth Sham <esham00@stuy.edu>
2018-12-09 13:30:27 -0500

    added madeSwap to bubble sort

--- Sorts.java
+++ Sorts.java
@@ -19,16 +19,22 @@ public class Sorts {
     //Sort by comparing the integer with all the integers in front of it and swapping indices if they are smaller
     public static void bubbleSort(int[] data) {
 	for(int i = 0; i < data.length; i++) {
-	    //looping through to see if the current int is smaller than the int after it 
+		boolean madeSwap = false;
+		//looping through to see if the current int is smaller than the int after it 
 	    for (int j = 0; j < data.length-i-1; j++) {
 		//choosing the value at the next index
 		int current = data[j];
-		//if the current int is smaller than the int after, it swap places
+	       	//if the current int is smaller than the int after, it swap places
 		if (current > data[j+1]) {
+		    madeSwap = true;
 		    int greater  = current;
 		    data[j] = data[j+1];
 		    data[j+1] = greater;
 		}
+	    }
+	    //ending checking early if they don't change places (which means that everything in order)
+	    if (madeSwap == false) {
+		i = data.length;
 	    }
 	}
     }
